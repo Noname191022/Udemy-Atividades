2197e012267c8f100e9cb2d89df615b64c31233d
Noname191022 <lucasmesquitarage92@outlook.com>
2026-06-28 18:23:40 -0400

    Atividade 1

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+System.out.println("Digite os dois valores a serem somados\n");
+int numero1 = scan.nextInt();
+int numero2 = scan.nextInt();
+
+int soma = numero2 + numero1;
+System.out.printf("A Soma dos valores é: %d", soma);
+    }
+}
