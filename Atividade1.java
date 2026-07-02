1b8771ec35ee184a130c6e53e8baa2bb6d342d9c
Noname191022 <lucasmesquitarage92@outlook.com>
2026-07-01 22:33:57 -0400

    atividades feitas e ajeitadas(Teste de IA Copilot)

---
+++ Atividade1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Atividade1{
+    public static void main(String[] args){
+        System.out.println("Digite o raio do circulo:\n");
+        Scanner scan = new Scanner(System.in);
+        float raio = scan.nextFloat();
+        double pi = 3.14159;
+        double result = pi * raio * raio;
+System.out.printf("Resultado: %f", result);
+
+    }
+}
